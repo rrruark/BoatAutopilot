659a7919ffbbcab980cc9b70fb1f934f9c733de8
Rob Ruark <70562660+rrruark@users.noreply.github.com>
2023-06-17 15:21:05 -0700

    Fixed compile errors and added debug statements.
    Code now loads onto board

--- BoatAutopilot.ino
+++ BoatAutopilot.ino
@@ -3,7 +3,10 @@
 #define HOME_CURRENT_THRESHOLD 2000
 #define MOTOR_POSITIVE_PIN 26
 #define MOTOR_NEGATIVE_PIN 27
-#define ENCODER_PIN_0 20
+#define ENCODER_PIN_0 22
+#define ENCODER_PIN_1 23
+
+int32_t setpoint_position = 0;
 
 volatile int32_t current_position = 0;
 
@@ -14,13 +17,15 @@ void IRAM_ATTR handlePulse()
 
 void setup()
 {
+  Serial.begin(9600);
+
   pinMode(MOTOR_POSITIVE_PIN, OUTPUT);
   pinMode(MOTOR_NEGATIVE_PIN, OUTPUT);
   
   pinMode(ENCODER_PIN_0, INPUT);
   pinMode(ENCODER_PIN_1, INPUT);
   
-  adc1_config_width(ADC_WIDTH_BIT_12);
+  //adc1_config_width(ADC_WIDTH_BIT_12);
   adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_0);
 
   attachInterrupt(digitalPinToInterrupt(ENCODER_PIN_0), handlePulse, RISING);
@@ -53,6 +58,11 @@ void HomeActuator()
   while (GetMotorCurrent() < HOME_CURRENT_THRESHOLD)
   {
     RunMotorNegative();
+    Serial.print("Measured Motor Current is: ");
+    Serial.println(GetMotorCurrent());
+    Serial.print("Current Position is: ");
+    Serial.println(current_position);
+    delay(500);
   }
   
   StopMotor();
@@ -74,6 +84,7 @@ void RunMotorNegative()
 {
   digitalWrite(MOTOR_POSITIVE_PIN, LOW);
   digitalWrite(MOTOR_NEGATIVE_PIN, HIGH);
+  Serial.println("Running motor negative");
 }
 
 void StopMotor()
